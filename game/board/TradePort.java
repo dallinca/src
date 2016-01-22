f2d100286bf2f948bfebc2469e7e7dd873514015
Dallin Carl Andersen <dallinca@iowa.cs.byu.edu>
2016-01-21 19:19:27 -0700

    Add in Board and Map classes

---
+++ game/board/TradePort.java
@@ -0,0 +1,27 @@
+package game.board;
+
+import shared.definitions.PortType;
+
+/**
+ * The TradePort class stores the kind of trade port available to improve
+ * trade rates for a player.
+ * 
+ * Operations include getting and setting the port type
+ *
+ */
+public class TradePort {
+
+	private PortType portType;
+	
+	public TradePort() {}
+
+	public PortType getPortType() {
+		return portType;
+	}
+
+	public void setPortType(PortType portType) {
+		this.portType = portType;
+	}
+	
+	
+}
---
+++ game/board/Vertex.java
@@ -0,0 +1,80 @@
+package game.board;
+
+import shared.locations.VertexLocation;
+
+/**
+ * The Vertex class is used to store whether a vertex has a municipal
+ * built on it.
+ * 
+ * Operations are provided for assigning the vertex a municipal, and
+ * checking whether there is a a municipal already assigned.
+ * 
+ *
+ */
+public class Vertex {
+	
+	Municipal municipal;
+	TradePort tradePort;
+	VertexLocation vertexLocation;
+	
+	public Vertex() {}
+	
+	/**
+	 * Returns whether the Vertex has a municipal
+	 * 
+	 * @pre None
+	 * 
+	 * @post Return value says whether the vertex has a municipal
+	 */
+	public boolean checkIfHasMunicipal() {
+		if(municipal == null) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Assigns the edge a municipality
+	 * 
+	 * @pre municipal == null, or municipal is a settlement, this check should happen at the board level
+	 * 
+	 * @post municipal = (Municipal)Object, the edge now has a player's road
+	 */
+	public void buildMunicipal(Municipal municipal) {
+		this.municipal = municipal;
+	}
+
+	/**
+	 * Returns whether the vertex has a tradePort
+	 * 
+	 * @pre None
+	 * 
+	 * @post Return value says whether the vertex has a tradePort
+	 */
+	public boolean hasTradePort() {
+		if(tradePort == null) {
+			return false;
+		}
+		return true;
+	}
+	
+	public TradePort getTradePort() {
+		return tradePort;
+	}
+
+	public void setTradePort(TradePort tradePort) {
+		this.tradePort = tradePort;
+	}
+
+	public VertexLocation getVertexLocation() {
+		return vertexLocation;
+	}
+
+	public void setVertexLocation(VertexLocation vertexLocation) {
+		this.vertexLocation = vertexLocation;
+	}
+	
+	
+	
+
+}
